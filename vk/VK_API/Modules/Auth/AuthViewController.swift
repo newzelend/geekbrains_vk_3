190bab1d5581623f980717772dda235b5a392bc8
newzelend <89086288+newzelend@users.noreply.github.com>
2021-08-18 14:22:43 +0300

    Update AuthViewController.swift

--- vk/VK_API/Modules/Auth/AuthViewController.swift
+++ vk/VK_API/Modules/Auth/AuthViewController.swift
@@ -18,7 +18,7 @@ class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       //190
+       
     }
     //sdfghjkl;
     func authorizeToVK() {
